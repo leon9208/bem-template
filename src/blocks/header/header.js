33a132fcbab8cce8bf4141a989f0b2ee3af8112f
Shakhrom <leon9208@mail.ru>
2019-12-14 00:43:51 +0300

    changed the WEBP DETECT script

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -5,17 +5,23 @@ let allEl = document.querySelectorAll('*')
     allEl.ontouchcancel = () => this.mouseover()
   }
 
-//webp detect
-let thisIsWebP = ()=> {
-  let def = $.Deferred(), crimg = new Image();
-  crimg.onload = ()=>  def.resolve()
-  crimg.onerror = ()=> def.reject()
-  crimg.src = "data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=";
-  return def.promise();
+//Проверка поддержки WEBP
+function check_webp_feature(feature, callback) {
+  let kTestImages = {
+      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+      alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+      animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+  };
+  let img = new Image();
+  img.onload = function () {
+      let result = (img.width > 0) && (img.height > 0);
+      callback(feature, result);
+  };
+  img.onerror = () => callback(feature, false);
+  img.src = "data:image/webp;base64," + kTestImages[feature];
 }
-
-thisIsWebP().then(function() {
-  document.body.classList.add('webp-support-js')
-}, function() {
-  document.body.classList.add('webp-nosupport-js')
+//Если браузер поддерживает WEBP, то добавляем класс тегу body
+check_webp_feature('lossy', function (feature, isSupported) {
+  isSupported ? document.body.classList.add('webp-support-js') : document.body.classList.add('webp-nosupport-js')
 });
