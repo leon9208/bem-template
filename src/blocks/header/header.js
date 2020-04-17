5462b7ded37020e94f1e9794b4293ed499b838fb
Shakhrom <leon9208@mail.ru>
2020-04-17 22:42:39 +0300

    Modified

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,21 +1,21 @@
 //fix hover element on mobile
 let allEl = document.querySelectorAll('*')
-  for(let i = 0; i < allEl.length; i++){
-    allEl.ontouchstart = () => this.mouseover()
-    allEl.ontouchcancel = () => this.mouseover()
-  }
+for(let i = 0; i < allEl.length; i++){
+  allEl.ontouchstart = () => this.mouseover()
+  allEl.ontouchcancel = () => this.mouseover()
+}
 
 //Проверка поддержки WEBP
 function check_webp_feature(feature, callback) {
-  let kTestImages = {
+  var kTestImages = {
       lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
       lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
       alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
       animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
   };
-  let img = new Image();
+  var img = new Image();
   img.onload = function () {
-      let result = (img.width > 0) && (img.height > 0);
+      var result = (img.width > 0) && (img.height > 0);
       callback(feature, result);
   };
   img.onerror = () => callback(feature, false);
